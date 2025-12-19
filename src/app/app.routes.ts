05396cc901bda8755005a6f7c09240d636a5d3ef
avilac26 <emanuelavilac@gmail.com>
2025-12-19 15:45:13 -0600

    restructuring of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartCard } from './start-card/start-card';
 import { SetGameCard } from './set-game-card/set-game-card';
-import { SetPlayersCard } from './set-players-card/set-players-card';
-import { SetImpostorsCard } from './set-impostors-card/set-impostors-card';
-import { SetCategoryCard } from './set-category-card/set-category-card';
-import { SetDurationCard } from './set-duration-card/set-duration-card';
+
 
 export const routes: Routes = [
     {path: '', redirectTo: '/start-card', pathMatch: 'full'},
     {path: 'start-card', component: StartCard},
     {path: 'set-game-card', component: SetGameCard},
-    {path: 'set-players-card', component: SetPlayersCard},
-    {path: 'set-impostors-card', component: SetImpostorsCard},
-    {path: 'set-category-card', component: SetCategoryCard},
-    {path: 'set-duration-card', component: SetDurationCard}
 ];
--- src/app/set-game-card/set-game-card.ts
+++ src/app/set-game-card/set-game-card.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-set-game-card',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './set-game-card.html',
   styleUrl: './set-game-card.css',
 })
 export class SetGameCard {
+
+  // Players var
+  name:string='';
+  players:string[]=[];
+  adding:boolean = false;
+
+
+
+  // Players logic
+
+  ngOnInit() {
+    const savedPlayers = localStorage.getItem('players');
+    if (savedPlayers) {
+      this.players = JSON.parse(savedPlayers);
+    }
+  }
+
+  add(){
+    this.adding =! this.adding;
+  }
+  addPlayer(){
+    this.players.push(this.name);
+    localStorage.setItem('players', JSON.stringify(this.players));
+    this.name = '';
+    this.adding =! this.adding;
+  }
+  deletePlayer(player:string){
+    let index = this.players.indexOf(player);
+    this.players.splice(index, 1);
+    localStorage.setItem('players', JSON.stringify(this.players));
+  }
 
 }
